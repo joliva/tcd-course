9bb634c8ee7217c6c418b51426882b1783ce8bc5
John Oliva <joliva63@gmail.com>
2013-09-12 17:27:21 -0400

    Completed Lab 12 for TCAD course. Integration of a webview with native controls.

--- lab12/webcontent/app/controllers/entryView.js
+++ lab12/webcontent/app/controllers/entryView.js
@@ -1,5 +1,12 @@
 var updateWebView = function() {
-	$.a.blur();$.b.blur();
+	$.a.blur();
+	$.b.blur();
+	
+	Ti.App.fireEvent('app:updatedValues', {
+		a: parseFloat($.a.value),
+		b: parseFloat($.b.value),
+		c: parseFloat($.c.value)
+	});
 };
 var calcC = function() {
 	$.c.value = String(Math.round(100 - $.a.value - $.b.value));
